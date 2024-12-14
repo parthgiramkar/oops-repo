9a4fc37cd1fc13e0e2d91d5a7c73691f0ccaed33
parthgiramkar <imparthgiramkar@gmail.com>
2024-12-14 19:40:04 +0530

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -1,53 +1,82 @@
-#include<iostream>
+#include <iostream>
+#include <string>
 using namespace std;
 
-class complex
-{
-    float real, imag;
+class Publication {
+protected:
+    string title;
+    float price;
 public:
-    complex(float r = 0, float i = 0) {
-        real = r;
-        imag = i;
+    Publication() : title(""), price(0.0) {}
+
+    void setData() {
+        cout << "Enter title: ";
+        getline(cin, title);
+        cout << "Enter price: ";
+        cin >> price;
+        cin.ignore(); // To ignore the newline character left in the buffer
     }
 
-    complex operator+(complex &c1) {
-        complex c3;
-        c3.real = real + c1.real;
-        c3.imag = imag + c1.imag;
-        return c3;
+    void display() const {
+        cout << "Title: " << title << endl;
+        cout << "Price: " << price << endl;
+    }
+};
+
+class Book : public Publication {
+private:
+    int pageCount;
+public:
+    Book() : pageCount(0) {}
+
+    void setData() {
+        Publication::setData();
+        cout << "Enter page count: ";
+        cin >> pageCount;
+        cin.ignore();
     }
 
-    complex operator*(complex &c1) {
-        complex c3;
-        c3.real = real * c1.real - imag * c1.imag;
-        c3.imag = real * c1.imag + imag * c1.real;
-        return c3;
+    void display() const {
+        Publication::display();
+        cout << "Page Count: " << pageCount << endl;
+    }
+};
+
+class Tape : public Publication {
+private:
+    float playingTime;
+public:
+    Tape() : playingTime(0.0) {}
+
+    void setData() {
+        Publication::setData();
+        cout << "Enter playing time (in minutes): ";
+        cin >> playingTime;
+        cin.ignore();
     }
 
-    friend istream & operator>>(istream &in, complex &c2) {
-        cout << "Enter the real part: ";
-        in >> c2.real;
-        cout << "Enter the imaginary part: ";
-        in >> c2.imag;
-        return in;
-    }
-
-    friend ostream & operator<<(ostream &out, complex &c2) {
-        out << c2.real << "+" << c2.imag << "i";
-        return out;
+    void display() const {
+        Publication::display();
+        cout << "Playing Time: " << playingTime << " minutes" << endl;
     }
 };
 
 int main() {
-    complex c1, c2, c3;
-    cout << "Enter the first complex number:" << endl;
-    cin >> c1;
-    cout << "Enter the second complex number:" << endl;
-    cin >> c2;
-    c3 = c1 + c2;
-    cout << "Sum: " << c3 << endl;
-    c3 = c1 * c2;
-    cout << "Product: " << c3 << endl;
+    Book book;
+    Tape tape;
+
+    cout << "Enter details for Book:" << endl;
+    book.setData();
+
+    cout << "\nEnter details for Tape:" << endl;
+    tape.setData();
+
+    cout << "\nDetails of Book:" << endl;
+    book.display();
+
+    cout << "\nDetails of Tape:" << endl;
+    tape.display();
+
     return 0;
 }
 
@@ -55,12 +84,40 @@ int main() {
 
 
 
+/*
+Inheritance:
+Inheritance in Object Oriented Programming can be described as a process of
+creating new classes from existing classes. New classes inherit some of the properties
+and behavior of the existing classes. An existing class that is "parent" of a new class is
+called a base class. New class that inherits properties of the base class is called a
+derived class. Inheritance is a technique of code reuse. It also provides possibility to
+extend existing classes by creating derived classes.
+    There are 3 access specifiers:
+Namely public, private and
+protected.
+public:
+This inheritance mode is used mostly. In this the protected member of
+Base classbecomes protected members of Derived class and public
+becomes public.
+protected:
+In protected mode, the public and protected members of Base class becomes
+protectedmembers of Derived class.
+private:
+In private mode the public and protected members of Base class become privatemembersof Derived class.
+    1. Single Inheritance
+2. Multiple Inheritance
+3. Hierarchical Inheritance
+4. Multilevel Inheritance
+5. Hybrid Inheritance
+Single Inheritance:
+In this type of inheritance one derived class inherits from only one base class.
+It is themost simplest form of Inheritance.
+
+    Multiple Inheritance: In this type of inheritance a single derived class may inherit from two or more than two base classes.
+    Hierarchical Inheritance:
+In this type of inheritance, multiple derived classes inherits from a single base class.
+    */
 
 
-Overloaded + operator: Adds two complex numbers by adding their real parts and imaginary parts separately.
-Overloaded * operator: Multiplies two complex numbers using the formula-
-(a+bi)(c+di)=(ac−bd)+(ad+bc)i
 
-Overloaded << operator: Prints a complex number in the form a + bi or a - bi.
-Overloaded >> operator: Reads the real and imaginary parts of a complex number from the input.
-  
+
